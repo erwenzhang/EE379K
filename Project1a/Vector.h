feb69a274f77e255591554d644114273f2ea7392
Anthony Weems <amlweems@gmail.com>
2015-02-02 02:36:29 -0600

    [1a] Pass all supplied test cases

--- Project1a/Vector.h
+++ Project1a/Vector.h
@@ -12,137 +12,176 @@ namespace epl{
 template <typename T>
 class vector {
 private:
-	T* data;
-	uint64_t length = 0;
+	T* head = nullptr;
+	T* tail = nullptr;
+	uint64_t hsize = 0;
+	uint64_t tsize = 0;
 	uint64_t capacity = 8;
 
-	void amor_double(void) {
-		T *tmp = data;
+	void amor_double(void)
+	{
+		T* hold = head;
+		T* told = tail;
 		capacity *= 2;
-		data = (T*)operator new(sizeof(T) * capacity);
-		for (uint64_t k = 0; k < length; k++) {
-			new (data + k)T(std::move(tmp[k]));
-			tmp[k].~T();
+		head = (T*)operator new(sizeof(T) * capacity);
+		tail = head + capacity - 1;
+		for (uint64_t k = 0; k < hsize; k++) {
+			new (head + k)T(std::move(hold[k]));
 		}
-		operator delete(tmp);
+		for (uint64_t k = 0; k < tsize; k++) {
+			new (tail - k)T(std::move(told[-k]));
+		}
+		operator delete(hold);
+	}
+	void copy(const vector<T>& that)
+	{
+		capacity = that.capacity;
+		hsize = that.hsize;
+		tsize = that.tsize;
+		head = that.head;
+		tail = that.tail;
+	}
+	T& lookup(uint64_t k)
+	{
+		if (k < tsize) return tail[k - tsize + 1];
+		else if (k < hsize + tsize) return head[k - tsize];
+		else throw std::out_of_range{"index out of range"};
 	}
 public:
 	vector(void)
 	{
-		data = (T*)operator new(sizeof(T) * capacity); 
+		head = (T*)operator new(sizeof(T) * capacity);
+		tail = head + capacity - 1;
 	}
 
-	explicit vector(uint64_t n) : length(0), capacity(n)
+	explicit vector(uint64_t n) : hsize(n), capacity(n)
 	{
 		if (n == 0) capacity = 8;
-		data = (T*)operator new(sizeof(T) * capacity); 
-		for (uint64_t k = 0; k < length; k++) {
-			new (data + k)T();
+		head = (T*)operator new(sizeof(T) * capacity);
+		tail = head + capacity;
+		for (uint64_t k = 0; k < hsize; k++) {
+			new (head + k)T();
 		}
 	}
 
 	~vector(void)
 	{
-		for (uint64_t k = 0; k < length; k++) {
-			data[k].~T();
+		for (uint64_t k = 0; k < hsize; k++) {
+			head[k].~T();
 		}
-		operator delete(data);
+		for (uint64_t k = 0; k < tsize; k++) {
+			tail[-k].~T();
+		}
+		operator delete(head);
 	}
 
 	vector(const vector<T>& that)
 	{
 		capacity = that.capacity;
-		length = that.length;
-		data = (T*)operator new(sizeof(T) * capacity);
-		for (uint64_t k = 0; k < length; k++) {
-			new (data + k)T(that.data[k]);
+		hsize = that.hsize;
+		tsize = that.tsize;
+		head = (T*)operator new(sizeof(T) * capacity);
+		tail = head + capacity;
+		for (uint64_t k = 0; k < hsize; k++) {
+			new (head + k)T(that.head[k]);
+		}
+		for (uint64_t k = 0; k < tsize; k++) {
+			new (tail - k)T(that.tail[-k]);
 		}
 	}
 
 	vector(const vector<T>&& that)
 	{
-		capacity = that.capacity;
-		length = that.length;
-		data = that.data;
+		copy(that);
 	}
 
-	vector<T>& operator=(const vector<T>& that)
+	vector<T>& operator=(vector<T>& that)
 	{
-		std::swap(data, that.data);
+		T* tmp = head;
+		copy(that);
+		that.head = tmp;
 		return *this;
 	}
 
-	vector<T>& operator=(const vector<T>&& that)
+	vector<T>& operator=(vector<T>&& that)
 	{
-		capacity = that.capacity;
-		length = that.length;
-		data = that.data;
-		that.length = 0;
-		that.data = 0;
-
+		if (this != &that) {
+			this->~vector();
+			copy(that);
+			that.head = nullptr;
+			that.hsize = 0;
+			that.tsize = 0;
+		}
 		return *this;
 	}
 
 	T& operator[](uint64_t k)
 	{
-		if (k < length) {
-			return data[k];
-		}
-		throw std::out_of_range{"index out of range"};
+		return lookup(k);
 	}
 
-	const T& operator[](uint64_t k) const {
-		if (k < length) {
-			return data[k];
-		}
-		throw std::out_of_range{"index out of range"};
+	const T& operator[](uint64_t k) const
+	{
+		return lookup(k);
 	}
 
-	uint64_t size(void) const {
-		return length;
+	uint64_t size(void) const
+	{
+		return hsize + tsize;
 	}
 
 	void push_back(const T& e)
 	{
-		if (length == capacity) {
+		if (hsize + tsize == capacity) {
 			amor_double();
 		}
-		new (data + length) T(e);
-		length++;
+		new (head + hsize) T(e);
+		hsize++;
 	}
 
 	void push_back(const T&& e)
 	{
-		if (length == capacity) {
+		if (hsize + tsize == capacity) {
 			amor_double();
 		}
-		new (data + length) T(std::move(e));
-		length++;
+		new (head + hsize) T(std::move(e));
+		hsize++;
 	}
-
-	void pop_back(void)
-	{
-		if (length == 0) {
-			throw std::out_of_range{"index out of range"};
-		}
-		length--;
-		data[length].~T();
-	}
-
 
 	void push_front(const T& e)
 	{
-		push_back(e);
+		if (hsize + tsize == capacity) {
+			amor_double();
+		}
+		new (tail - tsize) T(e);
+		tsize++;
 	}
 
 	void push_front(const T&& e)
 	{
-		push_back(e);
+		if (hsize + tsize == capacity) {
+			amor_double();
+		}
+		new (tail - tsize) T(std::move(e));
+		tsize++;
+	}
+
+	void pop_back(void)
+	{
+		if (hsize == 0) {
+			throw std::out_of_range{"index out of range"};
+		}
+		hsize--;
+		head[hsize].~T();
 	}
 
 	void pop_front(void)
 	{
-		pop_back();
+		if (tsize == 0) {
+			throw std::out_of_range{"index out of range"};
+		}
+		tsize--;
+		tail[-tsize].~T();
 	}
 };
 
