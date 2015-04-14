f8682d9420f9cb7030e52c0df4fa9c93604ea7ac
Anthony Weems <amlweems@gmail.com>
2015-04-14 14:33:20 -0500

    [2c] Lots of comments and code cleanup

--- Project2c/Valarray.h
+++ Project2c/Valarray.h
@@ -23,18 +23,20 @@ using epl::vector; // after submission
 
 namespace epl {
 
+/* declare these up front */
 template <typename T>
 struct valarray;
-
 template <typename T>
 struct vexpr;
 
-/*
- * type alias to detect numeric types
- */
+/* type alias to detect numeric types */
 template <typename T> struct is_complex : public std::false_type {};
 template <typename T> struct is_complex<std::complex<T>> : public std::true_type {};
 
+/*
+ * return true for all math-y types, could be more restrictive,
+ * but I like the idea of allowing unsigned long long
+ */
 template <typename T>
 using is_easy_math = typename std::enable_if<std::is_integral<T>::value || is_complex<T>::value>::type;
 
@@ -51,9 +53,15 @@ struct to_ref<valarray<T>> { using type = valarray<T>&; };
 template<typename T>
 using Ref = typename to_ref<T>::type;
 
+/* alias to grab a type's value_type */
 template <class T>
 using ValueType = typename T::value_type;
 
+/*
+ * given two types, try our best to make the right type choice
+ * this becomes tricky with complex types, which is why we use
+ * is_same so many times
+ */
 template <typename T, typename U>
 using SameSame = typename std::conditional<
 	std::is_same<T, const std::complex<double>>::value ||
@@ -72,6 +80,7 @@ using SameSame = typename std::conditional<
 	>::type
 >::type;
 
+/* given two types, choose the best promotion */
 template <class T, class U>
 using ConditionalComplex = typename std::conditional<
 	is_complex<T>::value,
@@ -83,9 +92,11 @@ using ConditionalComplex = typename std::conditional<
 	>::type
 >::type;
 
+/* shortcut for ConditionalComplex */
 template <class T, class U>
 using CondComp = ConditionalComplex<ValueType<T>, ValueType<U>>;
 
+/* a very special vexpr, returns the same value no matter what */
 template <class T>
 struct UnaryVal {
 	using value_type = T;
@@ -96,6 +107,7 @@ struct UnaryVal {
 	size_t size() const { return this->len(); }
 };
 
+/* unary function vexpr (used for things like negation, sqrt, etc.) */
 template <class Op, class Lhs>
 struct UnaryOp {
 	using value_type = CondComp<Lhs, Lhs>;
@@ -107,6 +119,7 @@ struct UnaryOp {
 	size_t size() const { return this->len(); }
 };
 
+/* binary function vexpr (used for things like addition, subtraction, etc.) */
 template <class Op, class Lhs, class Rhs>
 struct BinaryOp {
 	const Op op;
@@ -119,6 +132,7 @@ struct BinaryOp {
 	size_t size() const { return this->len(); }
 };
 
+/* unary function vexpr (used for things like apply) */
 template <template <class> class Op, class T, class Lhs>
 struct UnaryFunction {
 	const Op<T> op;
@@ -130,6 +144,11 @@ struct UnaryFunction {
 	size_t size() const { return this->len(); }
 };
 
+/* 
+ * unary and binary functions to use in our operator
+ * could have used std library functions except for
+ * the weird complex conversions.
+ */
 template <class T>
 struct unary_negate : std::unary_function<ValueType<T>, ValueType<T>> {
 	ValueType<T> operator()(const ValueType<T>& x) const { return -x; }
@@ -171,26 +190,28 @@ struct division : std::binary_function<ValueType<T>, ValueType<U>, ValueType<T>>
 	}
 };
 
+/* type aliases to verify template arguments are actually valarrays */
 template<class VExpr>
 struct is_vexpr : std::false_type {};
 template<class VExpr>
 struct is_vexpr<vexpr<VExpr>> : std::true_type {};
+template<typename T>
+struct is_vexpr<valarray<T>> : std::true_type {};
 template<typename U>
 using is_easy_vexpr = typename std::enable_if<is_vexpr<U>::value, U>::type;
 
+/* type aliases to help determine return types (and verify we have a valarray) */
 template<template<class, class> class Op, class A, class B>
 using BinOp = typename std::enable_if<is_vexpr<A>::value && is_vexpr<B>::value,
 	vexpr<BinaryOp<Op<A, B>, A, B>>>::type;
-
 template<template<class> class Op, class A>
 using UnOp = typename std::enable_if<is_vexpr<A>::value, vexpr<UnaryOp<Op<A>, A>>>::type;
-
 template <typename T>
 using UnVal = vexpr<UnaryVal<T>>;
+template <template<class> class Op, typename A, typename U>
+using UnFun = typename std::enable_if<is_vexpr<A>::value, vexpr<UnaryFunction<Op, U, A>>>::type;
 
-template <class Op, class A>
-using UnAcc = typename std::enable_if<is_vexpr<A>::value, typename Op::result_type>::type;
-
+/* valarray expression definition */
 template<class VExpr>
 struct vexpr {
 	using value_type = typename VExpr::value_type;
@@ -201,6 +222,7 @@ struct vexpr {
 	size_t len() const { return v.len(); }
 	size_t size() const { return this->len(); }
 
+	/* a silly little iterator to make vexpr iterable */
 	class iterator {
 	public:
 		using value_type = typename VExpr::value_type;
@@ -232,10 +254,10 @@ struct vexpr {
 		value_type operator*() const { return v.operator[](k); }
 		value_type operator[](size_t n) const { return v.operator[](k + n); }
 	};
-
 	iterator begin() { return iterator(v, 0); }
 	iterator end() { return iterator(v, this->len()); }
 
+	/* apply and accumulate template functions */
 	template <template <class> class Func, typename T>
 	auto accumulate(Func<T> f) -> typename decltype(f)::result_type {
 		typename decltype(f)::result_type acc(this->operator[](0));
@@ -244,9 +266,8 @@ struct vexpr {
 		}
 		return acc;
 	}
-
 	template <template <class> class Func, typename U>
-	vexpr<UnaryFunction<Func, U, vexpr<VExpr>>> apply(Func<U> f) {
+	UnFun<Func, vexpr<VExpr>, U> apply(Func<U> f) {
 		using Op = UnaryFunction<Func, U, vexpr<VExpr>>;
 		return vexpr<Op>(Op(f, *this));
 	}
@@ -261,19 +282,15 @@ struct valarray : public vector<T> {
 	valarray() : vector<T>() {}
 	explicit valarray(size_t n) : vector<T>(n) {}
 	valarray(std::initializer_list<T> il) : vector<T>(il) {}
-
 	size_t len() const { return this->size(); }
 
+	/* create a valarray from a vexpr */
 	template <typename U, typename = is_easy_vexpr<U>>
 	valarray(U v) {
-		for (int k = 0; (k < this->len()) && (k < v.len()); k++) {
-			this->operator[](k) = v[k];
-		}
-		for (int k = this->len(); k < v.len(); k++) {
-			this->push_back(v[k]);
-		}
+		this->operator=(v);
 	}
 
+	/* not technically correct, but close enough */
 	valarray& operator=(T x) {
 		while (this->len() != 0) {
 			this->pop_back();
@@ -284,6 +301,7 @@ struct valarray : public vector<T> {
 		return *this;
 	}
 
+	/* create a valarray from a vexpr */
 	template <typename U, typename = is_easy_vexpr<U>>
 	valarray& operator=(U v) {
 		for (int k = 0; k < this->len(); k++) {
@@ -307,82 +325,61 @@ struct valarray : public vector<T> {
 		}
 		return acc;
 	}
-
 	template <template <class> class Func, typename U>
-	vexpr<UnaryFunction<Func, U, valarray<T>>> apply(Func<U> f) {
+	UnFun<Func, valarray<T>, U> apply(Func<U> f) {
 		using Op = UnaryFunction<Func, U, valarray<T>>;
 		return vexpr<Op>(Op(f, *this));
 	}
-
 	auto sqrt() -> decltype(this->apply(unary_sqrt<T>())) { return this->apply(unary_sqrt<T>()); }
-	UnAcc<std::plus<T>, valarray<T>> sum() { return this->accumulate(std::plus<T>()); }
-
-	UnOp<unary_negate, valarray<T>> operator-();
-	template <typename U>
-	BinOp<addition, valarray<T>, vexpr<U>> operator+(vexpr<U> o);
-	template <typename U>
-	BinOp<subtraction, valarray<T>, vexpr<U>> operator-(vexpr<U> o);
-	template <typename U>
-	BinOp<multiplication, valarray<T>, vexpr<U>> operator*(vexpr<U> o);
-	template <typename U>
-	BinOp<division, valarray<T>, vexpr<U>> operator/(vexpr<U> o);
+	auto sum() -> decltype(this->accumulate(std::plus<T>())) { return this->accumulate(std::plus<T>()); }
 };
 
-template<typename T>
-struct is_vexpr<valarray<T>> : std::true_type {};
-
+/* the actual operators between valarrays */
 template<class Expr1>
 UnOp<unary_negate, Expr1> operator-(const Expr1& x) {
 	using Op = UnaryOp<unary_negate<Expr1>, Expr1>;
-
 	return vexpr<Op>(Op(unary_negate<Expr1>(), x));
 }
-
 template<class Expr1, class Expr2>
 BinOp<addition, Expr1, Expr2> operator+(const Expr1& x, const Expr2& y) {
 	using Op = BinaryOp<addition<Expr1, Expr2>, Expr1, Expr2>;
-
 	return vexpr<Op>(Op(addition<Expr1, Expr2>(), x, y));
 }
 template<class Expr1, class Expr2>
 BinOp<subtraction, Expr1, Expr2> operator-(const Expr1& x, const Expr2& y) {
 	using Op = BinaryOp<subtraction<Expr1, Expr2>, Expr1, Expr2>;
-
 	return vexpr<Op>(Op(subtraction<Expr1, Expr2>(), x, y));
 }
 template<class Expr1, class Expr2>
 BinOp<multiplication, Expr1, Expr2> operator*(const Expr1& x, const Expr2& y) {
 	using Op = BinaryOp<multiplication<Expr1, Expr2>, Expr1, Expr2>;
-
 	return vexpr<Op>(Op(multiplication<Expr1, Expr2>(), x, y));
 }
 template<class Expr1, class Expr2>
 BinOp<division, Expr1, Expr2> operator/(const Expr1& x, const Expr2& y) {
 	using Op = BinaryOp<division<Expr1, Expr2>, Expr1, Expr2>;
-
 	return vexpr<Op>(Op(division<Expr1, Expr2>(), x, y));
 }
 
+/* handle operations between valarrays and math-y numbers */
 template <typename T, typename U, typename = is_easy_math<U>>
 BinOp<addition, T, UnVal<U>> operator+(const T& a, const U& b) { return a + UnVal<U>(UnaryVal<U>(b)); }
 template <typename T, typename U, typename = is_easy_math<U>>
 BinOp<addition, UnVal<U>, T> operator+(const U& b, const T& a) { return UnVal<U>(UnaryVal<U>(b)) + a; }
-
 template <typename T, typename U, typename = is_easy_math<U>>
 BinOp<subtraction, T, UnVal<U>> operator-(const T& a, const U& b) { return a - UnVal<U>(UnaryVal<U>(b)); }
 template <typename T, typename U, typename = is_easy_math<U>>
 BinOp<subtraction, UnVal<U>, T> operator-(const U& b, const T& a) { return UnVal<U>(UnaryVal<U>(b)) - a; }
-
 template <typename T, typename U, typename = is_easy_math<U>>
 BinOp<multiplication, T, UnVal<U>> operator*(const T& a, const U& b) { return a * UnVal<U>(UnaryVal<U>(b)); }
 template <typename T, typename U, typename = is_easy_math<U>>
 BinOp<multiplication, UnVal<U>, T> operator*(const U& b, const T& a) { return UnVal<U>(UnaryVal<U>(b)) * a; }
-
 template <typename T, typename U, typename = is_easy_math<U>>
 BinOp<division, T, UnVal<U>> operator/(const T& a, const U& b) { return a / UnVal<U>(UnaryVal<U>(b)); }
 template <typename T, typename U, typename = is_easy_math<U>>
 BinOp<division, UnVal<U>, T> operator/(const U& b, const T& a) { return UnVal<U>(UnaryVal<U>(b)) / a; }
 
+/* allow the user to print valarrays */
 template <typename T, typename = is_easy_vexpr<T>>
 std::ostream& operator<<(std::ostream& stream, const T& x) {
 	char sep = '[';
